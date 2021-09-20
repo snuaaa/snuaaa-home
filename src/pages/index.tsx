29353eca9baddd16aaa105cc2f62f95d4c4b56bd
Enif <yjkim1803@gmail.com>
2021-09-20 23:08:47 +0900

    feat: make index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,37 +2,84 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import { css } from '@emotion/css';
+import logo from 'public/img/aaa.png';
+import stars from 'public/img/stars.png';
 
 const styles = {
+  wrapper: css({
+    height: '100%',
+    width: '100%',
+  }),
+  background: css({
+    position: 'absolute',
+    height: '100%',
+    width: '100%',
+    zIndex: -1,
+  }),
+  main: css({
+    display: 'flex',
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }),
   title: css({
-    fontSize: 20,
-  })
+    fontFamily: 'Cafe24Ssurround',
+    fontSize: '1.8rem',
+    textAlign: 'center',
+    color: '#FFFFFF',
+    margin: 0,
+  }),
+  logo: css({
+    height: 500,
+    width: 500,
+  }),
+  buttonWrp: css({
+    '& button': {
+      backgroundColor: 'rgba(255, 255, 255, 0.2)',
+      color: '#FFFFFF',
+      borderRadius: '1.5rem',
+      padding: '1rem 2rem 0.8rem 2rem',
+      margin: '0 1rem',
+      '&:hover': {
+        backgroundColor: '#4985A7',
+      },
+      '&:active': {
+        backgroundColor: '#4985A7',
+      },
+    },
+  }),
 };
 
 const Home: NextPage = () => (
-  <div>
+  <>
     <Head>
       <title>SNU AAA HOME</title>
     </Head>
-
-    <main>
-      <h1 className={styles.title}>
-        서울대학교 아마추어 천문회
-      </h1>
-    </main>
-    <footer>
-      <a
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by{' '}
-        <span>
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+    <span className={styles.background}>
+      <Image src={stars} layout="fill" alt="background" />
+    </span>
+    <div className={styles.wrapper}>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          서울대학교 아마추어 천문회
+        </h1>
+        <span className={styles.logo}>
+          <Image src={logo} width={500} height={500} alt="aaa" />
         </span>
-      </a>
-    </footer>
-  </div>
+        <div className={styles.buttonWrp}>
+          <button type="button">동아리 소개</button>
+          <a href="https://www.snuaaa.net/" rel="noopener noreferrer">
+            <button type="button">커뮤니티</button>
+          </a>
+          <a href="https://archive.snuaaa.net/" rel="noopener noreferrer">
+            <button type="button">옛 커뮤니티</button>
+          </a>
+        </div>
+      </main>
+    </div>
+  </>
 );
 
 export default Home;
