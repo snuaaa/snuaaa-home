fd849b447819a130a5d76b0a46a95edbd4a65775
sjhbread <sjhbread@naver.com>
2021-10-15 17:02:42 +0900

    'layout-set'

--- src/components/pages/intro.tsx
+++ src/components/pages/intro.tsx
@@ -2,66 +2,88 @@ import { css, cx } from '@emotion/css';
 import logo from 'public/img/logo.png';
 import Image from 'next/image';
 import photo from 'public/img/photo.png';
+import { RecoilBridge } from 'recoil';
+import usePage from 'src/hooks/usePage';
 const styles = {
   main: css({
-    
     color: '#FFFFFF',
     display: 'flex',
     width: '100%',
-    height: '100%'
+    height: '100%',
+    position: 'absolute',
   }),
   leftLayout: css({
-    display: 'flex',
     width: '50%',
     height: '100%',
-    
-    
+    display: 'table',
+    position: 'relative'
   }),
   rightLayout: css({
+    position: 'relative',
     width: '50%',
     height: '100%',
-    
-    
   }),
-  contentWrapper : css({
+  logoImageWrap: css({
+    padding: '1rem 0rem'
+  }),
+  contentWrap : css({
+    display: 'table-cell',
     width: '100%',
     height: '100%',
-    display: 'block',
-    alignItems: 'center',
-    justifyContent: 'center',
-  }),
-  contentText: css({
-    fontsize: '30'
+    padding: '5rem 10rem',
+    verticalAlign: 'middle',
     
   }),
+  contentText: css({
+    fontSize: '2rem',
+    fontFamily: '"Snowburst One", cursive',
+    padding: '0.5rem 0rem',
+    textAlign: 'left',
+    '&:first-letter': {
+      fontWeight: 'bold',
+    },
+  }),
   backText: css({
-
+    position: 'absolute',
+    width: 'fit-content',
+    fontSize: '18px',
+    margin: '5rem 1.5rem'
   }),
   explainText: css({
-
+    padding: '2rem 0rem',
+    fontSize: 'small',
+    float: 'left',
+    textAlign: 'justify',
   }),
   photoText: css({
-    position: 'relative'
+    float: 'right',
+    padding: '1rem',
+    position: 'relative',
+    color: '#9CDBFF',
+    fontSize: 'small',
+    zIndex: 100,
   }),
 };
 
 const Intro: React.FC = () => (
+  
   <>
     <div className={cx([styles.main])}>
+      <div className={styles.backText}>&lt; BACK</div>
       <div className={styles.leftLayout}>
-        <div className={styles.backText}>Back</div>
-        <div className={styles.contentWrapper}>
-          <Image src={logo} alt='logo' />
+        <div className={styles.contentWrap}>
+          <div className={styles.logoImageWrap}>
+            <Image width={100} height={100} src={logo} alt='logo' />
+          </div>
           <div className={styles.contentText}>Amateur</div>
-          <div className={styles.contentText}>Astroomy</div>
+          <div className={styles.contentText}>Astronomy</div>
           <div className={styles.contentText}>Association</div>
+          
           <div className={styles.explainText}>AAA는 아마추어 천문활동을 사랑하는 회원들이 모여 구성한 서울대학교의 중앙 동아리입니다. 1980년 자연대 동아리로 시작하여 약 40년이 지난 현재까지 계속되어 온만큼 유구한 역사를 자랑합니다.</div>
         </div>
       </div>
       <div className={styles.rightLayout}>
-        <Image src={photo} alt='AAA18 김현용 작'>
-          
-        </Image>
+        <Image layout='fill' objectFit='fill' src={photo} alt='AAA18 김현용 작'></Image>
         <div className={styles.photoText}>AAA18 김현용 작</div>
         
       </div>
