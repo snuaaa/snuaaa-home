60735c40d005e93e44cf12326b1829dc88b29d02
sjhbread <sjhbread@naver.com>
2021-10-15 17:47:38 +0900

    'final'

--- src/components/pages/intro.tsx
+++ src/components/pages/intro.tsx
@@ -2,8 +2,6 @@ import { css, cx } from '@emotion/css';
 import logo from 'public/img/logo.png';
 import Image from 'next/image';
 import photo from 'public/img/photo.png';
-import { RecoilBridge } from 'recoil';
-import usePage from 'src/hooks/usePage';
 const styles = {
   main: css({
     color: '#FFFFFF',
@@ -32,7 +30,6 @@ const styles = {
     height: '100%',
     padding: '5rem 10rem',
     verticalAlign: 'middle',
-    
   }),
   contentText: css({
     fontSize: '2rem',
@@ -40,14 +37,15 @@ const styles = {
     padding: '0.5rem 0rem',
     textAlign: 'left',
     '&:first-letter': {
-      fontWeight: 'bold',
+      textShadow: '1px 0 5px'
     },
   }),
   backText: css({
     position: 'absolute',
     width: 'fit-content',
     fontSize: '18px',
-    margin: '5rem 1.5rem'
+    margin: '5rem 1.5rem',
+    
   }),
   explainText: css({
     padding: '2rem 0rem',
@@ -63,6 +61,12 @@ const styles = {
     fontSize: 'small',
     zIndex: 100,
   }),
+  footer: css({
+    padding: 'calc(50% - 7rem) 1.5rem',
+    position: 'absolute',
+    fontSize: 'xx-small',
+    height: 'fit-content',
+  })
 };
 
 const Intro: React.FC = () => (
@@ -70,6 +74,11 @@ const Intro: React.FC = () => (
   <>
     <div className={cx([styles.main])}>
       <div className={styles.backText}>&lt; BACK</div>
+      <div className={styles.footer}>
+        <p>ⓒ 2021, 서울대학교 아마추어 천문회.</p>
+        <p>Amateur Astronomy Association, Since 1980</p>
+        <p>Designed by Syeon, Programmed by Enif</p>
+      </div>  
       <div className={styles.leftLayout}>
         <div className={styles.contentWrap}>
           <div className={styles.logoImageWrap}>
@@ -78,14 +87,13 @@ const Intro: React.FC = () => (
           <div className={styles.contentText}>Amateur</div>
           <div className={styles.contentText}>Astronomy</div>
           <div className={styles.contentText}>Association</div>
-          
           <div className={styles.explainText}>AAA는 아마추어 천문활동을 사랑하는 회원들이 모여 구성한 서울대학교의 중앙 동아리입니다. 1980년 자연대 동아리로 시작하여 약 40년이 지난 현재까지 계속되어 온만큼 유구한 역사를 자랑합니다.</div>
-        </div>
+        </div>  
+        
       </div>
       <div className={styles.rightLayout}>
         <Image layout='fill' objectFit='fill' src={photo} alt='AAA18 김현용 작'></Image>
         <div className={styles.photoText}>AAA18 김현용 작</div>
-        
       </div>
     </div>
   </>
