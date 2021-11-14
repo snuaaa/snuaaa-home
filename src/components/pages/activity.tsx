9f1fe86093d472f1212e2af094abd333bc7219e6
Enif <yjkim1803@gmail.com>
2021-11-14 15:12:43 +0900

    feat: make activityDetail component, ui fix

--- src/components/pages/activity.tsx
+++ src/components/pages/activity.tsx
@@ -2,7 +2,14 @@ import { css, cx } from '@emotion/css';
 import Image from 'next/image';
 import Page from 'src/components/page';
 import line from 'public/img/line.png';
-import { useState } from 'react';
+import { useCallback } from 'react';
+import imgDidimdol from 'public/img/didimdol.png';
+import imgByulmoim from 'public/img/byulmoim.png';
+import imgSogwuan from 'public/img/sogwuan.png';
+import imgChulsa from 'public/img/chulsa.png';
+import imgExhibition from 'public/img/sajinjeon.png';
+import imgOtherActivity from 'public/img/chinmokdomo.png';
+import ActivityDetail from './activityDetail';
 
 const styles = {
   main: css({ 
@@ -22,174 +29,78 @@ const styles = {
   imageWrapper: css({
     position: 'relative',
     display: 'block',
-    width: '40vw',
+    width: '50rem',
     paddingBottom: '7rem',
-  }),
-  imageStyle: css({
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: '100% 100%',
-    width: '2rem',
-    height: '2rem',
-    cursor: 'pointer',
-    position: 'absolute',
-    zIndex: 1,
-    '&:hover': {
-      backgroundSize: '100% 100%',
-      backgroundRepeat: 'no-repeat',
-    },
-  }),
-  imageStyle1: css({
-    background: 'url(/img/star1.png)',
-    left: '-3.5rem',
-    top: '-4rem',
-    '&:hover': {
-      background: 'url(/img/yellowstar1.png)',
-    },
-  }),
-  imageStyle2: css({
-    background: 'url(/img/star2.png)',
-    position: 'absolute',
-    left: '4.2rem',
-    top: '4.5rem',
-    '&:hover': {
-      background: 'url(/img/yellowstar2.png)',
-    },
-  }),
-  imageStyle3: css({
-    background: 'url(/img/star3.png)',
-    position: 'absolute',
-    left: '13.5rem',
-    top: '1rem',
-    '&:hover': {
-      background: 'url(/img/yellowstar3.png)',
-    },
-  }),
-  imageStyle4: css({
-    background: 'url(/img/star4.png)',
-    position: 'absolute',
-    left: '21.7rem',
-    top: '4.5rem',
-    '&:hover': {
-      background: 'url(/img/yellowstar4.png)',
-    },
-  }),
-  imageStyle5: css({
-    background: 'url(/img/star5.png)',
-    position: 'absolute',
-    left: '30rem',  
-    top: '-1rem',
-    '&:hover': {
-      background: 'url(/img/yellowstar5.png)',
-    },
-  }),
-  imageStyle6: css({
-    background: 'url(/img/star6.png)',
-    position: 'absolute',
-    right: '-3rem',
-    top: '3rem',
-    '&:hover': {
-      background: 'url(/img/yellowstar6.png)',
-    },
-  }),
-  fontStyle: css({
-    position: 'absolute',
-    fontFamily: 'Cafe24SsurroundAir',
-    textAlign: 'center',
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: '0.8rem',
-  }),
-  fontStyle1: css({
-    top: '2rem',
-    width: '5rem',
-    left: '-1.5rem',
-  }),
-  fontStyle2: css({
-    top: '2rem',
-    width: '5rem',
-    left: '-1.5rem',  
-  }),
-  fontStyle3: css({
-    top: '2rem',
-    width: '5rem',
-    left: '-1.5rem',
-  }),
-  fontStyle4: css({
-    top: '-2.5rem',
-    left: '-0.05rem',
-  }),
-  fontStyle5: css({
-    left: '-1.5rem',
-    width: '5rem',
-    top: '2rem',
-  }),
-  fontStyle6: css({
-    top: '2rem',
-    width: '5rem',
-    left: '-1.5rem',
-  }),
-  picture: css({
-    position: 'absolute',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: '100% 100%',
-    width: '10rem',
-    height: '6rem',
-    cursor: 'pointer',
-    zIndex: 2,
-  }),
-  picture1: css({
-    display: 'block',
-    background: 'url(/img/didimdol.png)',
-    left: '-7.5rem',
-    top: '-4rem',
-  }),
-  pictureHide: css({
-    display: 'none',
-  }),
-  picture2: css({
-    display: 'block',
-    background: 'url(/img/byulmoim.png)',
-    left: '0.2rem',
-    top: '4.5rem',
-  }),
-  picture3: css({
-    display: 'block',
-    background: 'url(/img/sogwuan.png)',
-    left: '9.5rem',
-    top: '1rem',
-  }),
-  picture4: css({
-    display: 'block',
-    background: 'url(/img/chulsa.png)',
-    left: '17.7rem',
-    top: '1.5rem',
-  }),
-  picture5: css({
-    display: 'block',
-    background: 'url(/img/sajinjeon.png)',
-    left: '26rem',  
-    top: '-1rem',
-  }),
-  picture6: css({
-    display: 'block',
-    background: 'url(/img/chinmokdomo.png)',
-    right: '-7rem',
-    top: '3rem',
-  }),
-  pictureFont: css({
-    position: 'absolute',
-    fontFamily: 'Cafe24SsurroundAir',
-    textAlign: 'center',
-    color: 'white',
-    fontSize: '0.5rem',
-    top: '6rem',
   }),
 };
 
+const details = [
+  {
+    index: 1,
+    left: '-3.5rem',
+    top: '-4rem',
+    name: '디딤돌',
+    detailImg: imgDidimdol,
+    detailDescription: 'AAA의 1년간의 천문 활동은 천체\n사진 작품으로 남아 매년 하반기\n사진전을 통해 대외에 공개합니다.',
+  },
+  {
+    index: 2,
+    left: '5.5rem',
+    top: '6rem',
+    name: '별모임',
+    detailImg: imgByulmoim,
+    detailDescription: '매주 열리는 별모임 활동을 통해\n아마추어 천문 실습을 합니다.',
+  },
+  {
+    index: 3,
+    left: '17.5rem',
+    top: '1rem',
+    name: '소규모\n관측회',
+    detailImg: imgSogwuan,
+    detailDescription: '날씨가 좋은 날엔 도심을 떠나\n산 속에서 별을 보는 소규모 관측회가 열립니다.',
+  },
+  {
+    index: 4,
+    left: '28rem',
+    top: '6rem',
+    name: '출사',
+    detailImg: imgChulsa,
+    detailDescription: '학교 근처 및 서울 근교에서 천체\n사진을 촬영해보는 활동입니다.',
+  },
+  {
+    index: 5,
+    left: '38.5rem',
+    top: '-3rem',
+    name: '사진전',
+    detailImg: imgExhibition,
+    detailDescription: 'AAA의 1년간의 천문 활동은 천체\n사진 작품으로 남아 매년 하반기\n사진전을 통해 대외에 공개합니다.',
+  },
+  {
+    index: 6,
+    right: '-4rem',
+    top: '4rem',
+    name: '친목활동',
+    detailImg: imgOtherActivity,
+    detailDescription: '여름, 겨울 관측회 및 기타\n다양한 친목도모 활동을 합니다!',
+  },
+];
+
 const Activity: React.FC = () => {
-  const [isClicked, setIsClicked] = useState([0,0,0,0,0,0]);
+  const makeActivityDetails = useCallback(() => details.map((detail) => (
+    <ActivityDetail
+      key={`detail-${detail.index}`}
+      index={detail.index}
+      top={detail.top}
+      left={detail.left}
+      right={detail.right}
+      name={detail.name || ''}
+      detailImg={detail.detailImg}
+      detailDescription={detail.detailDescription}
+    />
+  )), []);
+  
   return (
-  <Page>
+    <Page>
       <div className={cx([styles.main])}>
         <div className={styles.contentWrapper}> 
           <p className={styles.title}>Our Activities</p>
@@ -197,48 +108,15 @@ const Activity: React.FC = () => {
           <p>별들을 클릭하여 AAA의 다양한 활동들을 알아보세요!</p>
         </div>
         <div className={styles.imageWrapper}>
-          <button type='button' onClick={() => setIsClicked([1,0,0,0,0,0])} className={cx([styles.imageStyle1, styles.imageStyle])}> 
-            <p className={cx([styles.fontStyle, styles.fontStyle1])}>디딤돌<br/>교육</p>
-          </button>
-          <button type='button' onClick={() => setIsClicked([0,0,0,0,0,0])} className={isClicked[0]? cx([styles.picture1, styles.picture]) : cx([styles.pictureHide, styles.picture])}>
-            <p className={cx([styles.pictureFont])}>AAA의 1년간의 천문 활동은 천체 <br/> 사진 작품으로 남아 매년 하반기 <br/> 사진전을 통해 대외에 공개합니다.</p>
-          </button>
-          <button type='button' onClick={() => setIsClicked([0,1,0,0,0,0])} className={cx([styles.imageStyle2, styles.imageStyle])}>
-            <p className={cx([styles.fontStyle, styles.fontStyle2])}>별모임</p>
-          </button>
-          <button type='button' onClick={() => setIsClicked([0,0,0,0,0,0])} className={isClicked[1]? cx([styles.picture2, styles.picture]) : cx([styles.pictureHide, styles.picture])}>
-            <p className={cx([styles.pictureFont])}>매주 열리는 별모임 활동을 통해 <br/> 아마추어 천문 실습을 합니다.</p>
-          </button>
-          <button type='button' onClick={() => setIsClicked([0,0,1,0,0,0])} className={cx([styles.imageStyle3, styles.imageStyle])}>
-            <p className={cx([styles.fontStyle, styles.fontStyle3])}>소규모<br/>관측회</p>
-          </button>
-          <button type='button' onClick={() => setIsClicked([0,0,0,0,0,0])} className={isClicked[2]? cx([styles.picture3, styles.picture]) : cx([styles.pictureHide, styles.picture])}>
-            <p className={cx([styles.pictureFont])}>날씨가 좋은 날엔 도심을 떠나 <br/> 산 속에서 별을 보는 소규모 관측회가 열립니다. </p>
-          </button>
-          <button type='button' onClick={() => setIsClicked([0,0,0,1,0,0])} className={cx([styles.imageStyle4, styles.imageStyle])}>
-            <p className={cx([styles.fontStyle, styles.fontStyle4])}>출사</p>
-          </button>
-          <button type='button' onClick={() => setIsClicked([0,0,0,0,0,0])} className={isClicked[3]? cx([styles.picture4, styles.picture]) : cx([styles.pictureHide, styles.picture])}>
-            <p className={cx([styles.pictureFont])}> 학교 근처 및 서울 근교에서 천체 <br/> 사진을 촬영해보는 활동입니다.</p>
-          </button>
-          <button type='button' onClick={() => setIsClicked([0,0,0,0,1,0])} className={cx([styles.imageStyle5, styles.imageStyle])}>
-            <p className={cx([styles.fontStyle, styles.fontStyle5])}>사진전</p>
-          </button>
-          <button type='button' onClick={() => setIsClicked([0,0,0,0,0,0])} className={isClicked[4]? cx([styles.picture5, styles.picture]) : cx([styles.pictureHide, styles.picture])}>
-            <p className={cx([styles.pictureFont])}>AAA의 1년 간의 친목 활동은 천체 <br/> 사진 작품으로 남아 매년 하반기 <br/> 사진전을 통해 대외에 공개합니다.</p>
-          </button>
-          <button type='button' onClick={() => setIsClicked([0,0,0,0,0,1])} className={cx([styles.imageStyle6, styles.imageStyle])}>
-            <p className={cx([styles.fontStyle, styles.fontStyle6])}>친목활동</p>
-          </button>
-          <button type='button' onClick={() => setIsClicked([0,0,0,0,0,0])} className={isClicked[5]? cx([styles.picture6, styles.picture]) : cx([styles.pictureHide, styles.picture])}>
-            <p className={cx([styles.pictureFont])}>여름, 겨울 관측회 및 기타 <br/> 다양한 친목도모 활동을 합니다!</p>
-          </button>
+          {
+            makeActivityDetails()
+          }
           <Image src={line} alt='line'/>
         </div>
       </div>
     </Page>
-    );
-  };
+  );
+};
   
   
 
